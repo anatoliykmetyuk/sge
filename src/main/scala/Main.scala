74159e4108715780ca9fe5017d9d79c3df77df14
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-11-17 18:02:03 +0200

    Plain scala testing method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,9 +6,19 @@ import subscript.vm.executor._
 
 import scala.swing.Swing
 
+import org.lwjgl.opengl.GL11
+import org.lwjgl.glfw.GLFW._
+
 import sge._
 
 object Main extends Game with GameApp {
+
+  override def body() {
+    while(glfwWindowShouldClose(window) != GL11.GL_TRUE) {
+      glfwSwapBuffers(window)
+      glfwPollEvents()
+    }
+  }
 
   script live = println: "Live start!"
                 sleep: 3000
