0ccd94e72092f0e3a4f863113f2bf18013b98a18
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-11-17 17:35:52 +0200

    LWJGL update to GL3 begins

--- src/main/scala/sge/LWJGLEngine.scala
+++ src/main/scala/sge/LWJGLEngine.scala
@@ -1,12 +1,14 @@
 package sge
 
+import java.nio.ByteBuffer
+
 import org.lwjgl.system.MemoryUtil.NULL
 
-import org.lwjgl.glfw.{Callbacks, GLFWErrorCallback, GLFWKeyCallback}
+import org.lwjgl.glfw.{Callbacks, GLFWErrorCallback, GLFWKeyCallback, GLFWvidmode}
 import org.lwjgl.glfw.GLFW._
 
 import org.lwjgl.opengl.GLContext
-import org.lwjgl.opengl.GL11._
+import org.lwjgl.opengl.{GL11, GL15, GL20, GL32}
 
 
 trait LWJGLEngine {this: Game =>
@@ -21,30 +23,59 @@ trait LWJGLEngine {this: Game =>
   val keyCb = new GLFWKeyCallback {
     override def invoke(window: Long, key: Int, scancode: Int, action: Int, mods: Int) {
       if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE)
+        glfwSetWindowShouldClose(window, GL11.GL_TRUE)
     }
   }
 
   // Initializes OpenGL
   def initGl() {
+    // Setting the error callback, initializing OpenGL
     glfwSetErrorCallback(errorCb)
-    if (glfwInit() != GL_TRUE) throw new IllegalStateException("Unable to initialize GLFW")
+    if (glfwInit() != GL11.GL_TRUE) throw new IllegalStateException("Unable to initialize GLFW")
     
+    // Creating the window
+    glfwDefaultWindowHints()
+    glfwWindowHint(GLFW_VISIBLE, GL11.GL_FALSE)
     window = glfwCreateWindow(width, height, title, NULL, NULL)
+
     if (window == NULL) {
       glfwTerminate()
       throw new RuntimeException("Failed to create a window")
     }
+
+    // Setting the keyboard callback
     glfwSetKeyCallback(window, keyCb)
-    
+
+    // Centering the window
+    val vidmode: ByteBuffer = glfwGetVideoMode(glfwGetPrimaryMonitor())
+
+    glfwSetWindowPos(
+        window,
+        (GLFWvidmode.width(vidmode) - width) / 2,
+        (GLFWvidmode.height(vidmode) - height) / 2
+    )
+
+    // Make the OpenGL context current
     glfwMakeContextCurrent(window)
+
+    // Enable v-sync
+    glfwSwapInterval(1)
+
+    // Make the window visible
+    glfwShowWindow(window)
     GLContext.createFromCurrent()
+
+    // Set the clear color to blue
+    GL11.glClearColor(0.0f, 0.0f, 0.5f, 1.0f)
   }
 
   // Terminates OpenGL
   def termGl() {
+    // Destroying the window, releasing its keyboard callback
     glfwDestroyWindow(window)
     keyCb.release()
+
+    // Destroying GLFW, releasing its error callback
     glfwTerminate()
     errorCb.release()
   }
