f9086f07b643b7d1a4e0fba49746bc7acb840292
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-11-17 17:55:25 +0200

    Shader's programs

--- src/main/scala/sge/ShaderEngine.scala
+++ src/main/scala/sge/ShaderEngine.scala
@@ -5,16 +5,39 @@ import org.lwjgl.opengl.{GL11, GL20}
 trait ShaderEngine {
 
   def createShader(shader: Shader): Int = {
+    // Compile the shader
     val handle: Int = GL20.glCreateShader(shader.tpe)
     GL20.glShaderSource(handle, shader.source)
     GL20.glCompileShader(handle)
 
+    // Check for errors
     val status = GL20.glGetShaderi(handle, GL20.GL_COMPILE_STATUS)
     if (status == GL11.GL_FALSE) {
       val error = GL20.glGetShaderInfoLog(handle)
       System.err.println(s"Error while compiling shader $shader: $error")
     }
 
+    // Return shader's address in GPU memory
+    handle
+  }
+
+  def createShaderProgram(shaders: Array[Int]): Int = {
+    // Link shaders into a program
+    val handle: Int = GL20.glCreateProgram()
+    for (s <- shaders) GL20.glAttachShader(handle, s)
+    GL20.glLinkProgram(handle)
+
+    // Check for errors
+    val status = GL20.glGetShaderi(handle, GL20.GL_LINK_STATUS)
+    if (status == GL11.GL_FALSE) {
+      val error = GL20.glGetProgramInfoLog(handle)
+      System.err.println(s"Error while linking the program: $error")
+    }
+
+    // Detach shaders
+    for (s <- shaders) GL20.glDetachShader(handle, s)
+
+    // Reutrn program's address
     handle
   }
 
